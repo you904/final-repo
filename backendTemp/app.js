97eca92112b45d2e1bd5a03544fc7d3b1891ec18
syed mutahir <89296287+you904@users.noreply.github.com>
2024-08-12 23:51:42 -0700

    Update app.js

--- backendTemp/app.js
+++ backendTemp/app.js
@@ -22,7 +22,7 @@ const MONGODB_URI = process.env.URI_MONGO;
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error('Could not connect to MongoDB..', err));
-app.get("/",async(req,res)=>{
+app.use("/",async(req,res)=>{
   res.send("Hello")
 })
 app.use(express.static('public'));
